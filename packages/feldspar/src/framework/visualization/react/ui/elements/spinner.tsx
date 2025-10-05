ba64f3eea22b8cf0c5a54eda0f5e726bd07c015e
Tijn Schmits <tschmits@users.noreply.github.com>
2025-10-05 21:22:42 +0200

    Cleans up the script and moves code to modules

--- packages/feldspar/src/framework/visualization/react/ui/elements/spinner.tsx
+++ packages/feldspar/src/framework/visualization/react/ui/elements/spinner.tsx
@@ -29,7 +29,7 @@ export const Spinner = ({
           cy="12"
           r="10"
           stroke="currentColor"
-          stroke-width="4"
+          strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
