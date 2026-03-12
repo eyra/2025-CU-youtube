d75fa380db81f5a3637eb8903fc6dddde128a6de
Tijn Schmits <tschmits@users.noreply.github.com>
2026-03-12 18:27:07 +0100

    Sync feldspar by bringing in logging infrastructure and UI component updates

--- packages/feldspar/src/framework/visualization/react/ui/elements/spinner.tsx
+++ packages/feldspar/src/framework/visualization/react/ui/elements/spinner.tsx
@@ -18,7 +18,7 @@ export const Spinner = ({
   return (
     <div id="spinner" className="flex items-center justify-center">
       <svg
-        className={`animate-spin -ml-1 mr-3 h-5 w-5 ${textColor}`}
+        className={`animate-spin w-4 h-4 ${textColor}`}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
